18224b5b5c75bd931fd77bbe4db445c5c3467d2e
BeckerVM <beiker2203@gmail.com>
2019-03-01 14:52:48 -0500

    Añadiendo Banner, Header y Footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent
   ]
 })
 export class SharedModule { }
