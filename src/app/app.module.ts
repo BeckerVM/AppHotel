8fce1ee5d9765188e9391927a00803a2d4a94a84
Juan Manuel <juan.15121001@gmail.com>
2019-03-01 02:25:54 -0500

    añadiendo boletines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BulletinComponent } from './components/bulletin/bulletin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BulletinComponent
   ],
   imports: [
     BrowserModule
